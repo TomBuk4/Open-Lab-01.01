7ae0f89703f6ecf95a3246e714092f088e20d889
TomBuk4 <104893605+TomBuk4@users.noreply.github.com>
2022-09-19 18:09:33 +0200

    Update Exercise.cs

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,6 +6,14 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
+            int num_input = int32.Parse(Input);
+                
+            if (num_input < 10) {
+                return true;
+            }
+            else {
+                return False;
+                
             throw new NotImplementedException();
         }
     }
